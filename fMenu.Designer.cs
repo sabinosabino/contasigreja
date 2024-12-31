6c3e763cc1c2241093cf47847fa5efd50e84a7ea
sabino <sabinowelbert@gmail.com>
2024-12-30 23:09:26 -0300

    publish

--- fMenu.Designer.cs
+++ fMenu.Designer.cs
@@ -31,10 +31,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMenu));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.cadastrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.lançamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tiposToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.pessoasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lançamentosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.cmdlogar = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,6 +61,20 @@
             this.cadastrosToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
             this.cadastrosToolStripMenuItem.Text = "Cadastros";
             // 
+            // tiposToolStripMenuItem
+            // 
+            this.tiposToolStripMenuItem.Name = "tiposToolStripMenuItem";
+            this.tiposToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.tiposToolStripMenuItem.Text = "Tipos";
+            this.tiposToolStripMenuItem.Click += new System.EventHandler(this.tiposToolStripMenuItem_Click);
+            // 
+            // pessoasToolStripMenuItem
+            // 
+            this.pessoasToolStripMenuItem.Name = "pessoasToolStripMenuItem";
+            this.pessoasToolStripMenuItem.Size = new System.Drawing.Size(115, 22);
+            this.pessoasToolStripMenuItem.Text = "Pessoas";
+            this.pessoasToolStripMenuItem.Click += new System.EventHandler(this.pessoasToolStripMenuItem_Click);
+            // 
             // lançamentosToolStripMenuItem
             // 
             this.lançamentosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -66,32 +83,45 @@
             this.lançamentosToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
             this.lançamentosToolStripMenuItem.Text = "Lançamentos";
             // 
-            // tiposToolStripMenuItem
-            // 
-            this.tiposToolStripMenuItem.Name = "tiposToolStripMenuItem";
-            this.tiposToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.tiposToolStripMenuItem.Text = "Tipos";
-            this.tiposToolStripMenuItem.Click += new System.EventHandler(this.tiposToolStripMenuItem_Click);
-            // 
-            // pessoasToolStripMenuItem
-            // 
-            this.pessoasToolStripMenuItem.Name = "pessoasToolStripMenuItem";
-            this.pessoasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.pessoasToolStripMenuItem.Text = "Pessoas";
-            this.pessoasToolStripMenuItem.Click += new System.EventHandler(this.pessoasToolStripMenuItem_Click);
-            // 
             // contasToolStripMenuItem
             // 
             this.contasToolStripMenuItem.Name = "contasToolStripMenuItem";
-            this.contasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.contasToolStripMenuItem.Size = new System.Drawing.Size(111, 22);
             this.contasToolStripMenuItem.Text = "Contas";
             this.contasToolStripMenuItem.Click += new System.EventHandler(this.contasToolStripMenuItem_Click);
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(259, 114);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(308, 20);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(259, 152);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(308, 20);
+            this.txtSenha.TabIndex = 2;
+            // 
+            // cmdlogar
+            // 
+            this.cmdlogar.Location = new System.Drawing.Point(259, 189);
+            this.cmdlogar.Name = "cmdlogar";
+            this.cmdlogar.Size = new System.Drawing.Size(308, 23);
+            this.cmdlogar.TabIndex = 3;
+            this.cmdlogar.Text = "Logar";
+            this.cmdlogar.UseVisualStyleBackColor = true;
+            this.cmdlogar.Click += new System.EventHandler(this.cmdlogar_Click);
             // 
             // fMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdlogar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtLogin);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -114,5 +144,8 @@
         private System.Windows.Forms.ToolStripMenuItem pessoasToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem lançamentosToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem contasToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button cmdlogar;
     }
 }
--- fMenu.cs
+++ fMenu.cs
@@ -12,9 +12,15 @@ namespace ControleDizimoOferta
 {
     public partial class fMenu : Form
     {
+        
         public fMenu()
         {
             InitializeComponent();
+            this.menuStrip1.Visible = Staticos.logado;
+
+            this.txtLogin.Visible = !Staticos.logado;
+            this.txtSenha.Visible = !Staticos.logado;
+            this.cmdlogar.Visible = !Staticos.logado;
         }
 
         private void tiposToolStripMenuItem_Click(object sender, EventArgs e)
@@ -31,6 +37,22 @@ namespace ControleDizimoOferta
         private void contasToolStripMenuItem_Click(object sender, EventArgs e)
         {
             new fContas().ShowDialog();
+        }
+
+        private void cmdlogar_Click(object sender, EventArgs e)
+        {
+            if(txtLogin.Text.ToUpper()=="IGREJA" && txtSenha.Text == "igreja123456")
+            {
+                Staticos.logado = true;
+                this.menuStrip1.Visible = Staticos.logado;
+
+                this.txtLogin.Visible = !Staticos.logado;
+                this.txtSenha.Visible = !Staticos.logado;
+                this.cmdlogar.Visible = !Staticos.logado;
+                return;
+            }
+            MessageBox.Show("Login ou senha inválidos");
+
         }
     }
 }
